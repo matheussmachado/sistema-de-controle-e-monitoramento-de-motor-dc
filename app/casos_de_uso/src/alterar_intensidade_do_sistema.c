d0e45536ba426a7e9c809059a6067bf824663558
matheus machado <machadom@outlook.com.br>
2022-07-07 23:35:09 -0300

    ultimo commit antes da migração

--- app/casos_de_uso/src/alterar_intensidade_do_sistema.c
+++ app/casos_de_uso/src/alterar_intensidade_do_sistema.c
@@ -6,10 +6,9 @@
 
 // EXEMPLO DE UM CASO DE USO
 void alterar_intensidade_do_sistema(char porcentagem) {  
-  // EXEMPLO DE VALIDAÇÃO DE REQUISIÇÃO EXTERNA AO SISTEMA
-  if (porcentagem >= PORCENTAGEM_MAX)
-    // EXEMPLO DE REQUISIÇÃO DO SISTEMA AO EXTERIOR
-    if (botao_limitador.esta_acionado())
+  // EXEMPLO DE RESPOSTA INTERNA DO SITEMA -> TRATAMENTO DE REQUISIÇÃO EXTERNA
+  if (porcentagem >= PORCENTAGEM_MAX)    
+    if (botao_limitador.esta_acionado()) // EXEMPLO DE REQUISIÇÃO DO SISTEMA AO EXTERIOR
       porcentagem = PORCENTAGEM_MAX;
   
   // ABAIXO VÃO CHAMADAS PARA DISPOSITIVOS QUE PRECISAM SER ATUALIZADOS COM ESSA INFORMAÇÃO
